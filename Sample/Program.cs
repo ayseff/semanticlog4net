a1d2ad6c69bb030df0b90825cd5ba8e507eb078c
mcarr <mcarr@carr-engineering.com>
2013-04-29 12:33:20 -0400

    Initial commit

---
+++ Sample/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+using SemanticLog4Net.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var formatter = new Log4NetEventTextFormatter();
+			var listener = new ObservableEventListener();
+			listener.EnableEvents(SampleEventSource.Log, EventLevel.LogAlways, Keywords.All);
+			listener.LogToFlatFile("test.xml", formatter, true);
+
+			SampleEventSource.Log.ApplicationStarting();
+
+			for (int i = 0; i < 20; i++)
+				SampleEventSource.Log.SomeOtherEvent(i);
+
+			try
+			{
+				throw new Exception("Uh oh, watch out");
+			}
+			catch (Exception e)
+			{
+				SampleEventSource.Log.Error(e);
+			}
+
+			SampleEventSource.Log.ApplicationStopping();
+		}
+	}
+
+	class SampleEventSource : EventSource
+	{
+		public static SampleEventSource Log = new SampleEventSource();
+
+		[Event(1, Message = "Starting up.")]
+		public void ApplicationStarting()
+		{
+			this.WriteEvent(1);
+		}
+
+		[Event(2, Message = "Shutting down.")]
+		public void ApplicationStopping()
+		{
+			this.WriteEvent(2);
+		}
+
+		[Event(3, Message = "Here's something amazing.")]
+		public void SomeOtherEvent(int param)
+		{
+			this.WriteEvent(3, param);
+		}
+
+		[Event(4, Message = "Oops a problem!")]
+		public void Error(string exception)
+		{
+			this.WriteEvent(4, exception);
+		}
+
+		[NonEvent]
+		public void Error(Exception exception)
+		{
+			this.Error(exception.ToString());
+		}
+	}
+}
---
+++ SemanticLog4Net/Log4NetEventTextFormatter.cs
@@ -0,0 +1,88 @@
+﻿using log4net;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository;
+using log4net.Util;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Formatters;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Tracing;
+using System.Reflection;
+
+namespace SemanticLog4Net.Formatters
+{
+	public class Log4NetEventTextFormatter : IEventTextFormatter
+	{
+		readonly XmlLayout xmlLayout;
+		readonly ILoggerRepository repository;
+
+		public Log4NetEventTextFormatter()
+		{
+			this.xmlLayout = new XmlLayout();
+			this.repository = LogManager.GetRepository(Assembly.GetCallingAssembly());
+		}
+
+		public void WriteEvent(EventEntry eventEntry, System.IO.TextWriter writer)
+		{
+			var loggingEventData = new LoggingEventData
+			{
+				Level = this.GetLevel(eventEntry.Schema.Level),
+				LoggerName = eventEntry.Schema.TaskName,
+				Message = eventEntry.FormattedMessage,
+				TimeStamp = eventEntry.Timestamp.DateTime,
+			};
+
+			if (eventEntry.Payload.Count > 0)
+			{
+				loggingEventData.Properties = this.GetPropertiesDictionary(eventEntry.Schema.Payload, eventEntry.Payload);
+				loggingEventData.ExceptionString = this.GetExceptionString(eventEntry.Schema.Payload, eventEntry.Payload);
+			}
+
+			var loggingEvent = new LoggingEvent(null, this.repository, loggingEventData);
+			this.xmlLayout.Format(writer, loggingEvent);
+		}
+
+		PropertiesDictionary GetPropertiesDictionary(string[] payloadNames, ReadOnlyCollection<object> payloadValues)
+		{
+			var propertiesDictionary = new log4net.Util.PropertiesDictionary();
+			for (int i = 0; i < payloadNames.Length; i++)
+				propertiesDictionary[payloadNames[i]] = payloadValues[i];
+			return propertiesDictionary;
+		}
+
+		string GetExceptionString(string[] payloadNames, ReadOnlyCollection<object> payloadValues)
+		{
+			for (int i = 0; i < payloadNames.Length; i++)
+				if (payloadNames[i] == "exception")
+				{
+					var exception = payloadValues[i] as string;
+					if (exception != null)
+						return exception;
+				}
+			return null;
+		}
+
+		Level GetLevel(EventLevel level)
+		{
+			switch (level)
+			{
+				case EventLevel.Critical:
+					return Level.Critical;
+				case EventLevel.Error:
+					return Level.Error;
+				case EventLevel.Informational:
+					return Level.Info;
+				case EventLevel.LogAlways:
+					return Level.Off;
+				case EventLevel.Verbose:
+					return Level.Trace;
+				case EventLevel.Warning:
+					return Level.Warn;
+				default:
+					throw new InvalidOperationException("Unknown EventLevel.");
+			}
+		}
+	}
+
+}
